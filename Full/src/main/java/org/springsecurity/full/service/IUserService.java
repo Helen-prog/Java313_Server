c068b6f363c1bae30877373ebd7c0d5187eea818
Helen <elena.kozovyakina@gmail.com>
2024-11-06 17:16:11 +0300

    springSecurity

--- Full/src/main/java/org/springsecurity/full/service/IUserService.java
+++
@@ -1,9 +0,0 @@
-package org.springsecurity.full.service;
-
-import org.springsecurity.full.entity.User;
-
-public interface IUserService {
-    public User saveUser(User user);
-
-    public void removeSessionMessage();
-}
